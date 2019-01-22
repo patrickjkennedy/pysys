b7e94e2f36fe05fd5688d37258f6f26c77686fda
patrickjkennedy <patrickken@gmail.com>
2019-01-22 15:28:49 +0000

    created setup.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Pysys - System Information
+# Pysys - System Information
 
 This package was created as part of Practical 1 of UCD's Software Engineering course, COMP30830.
 It returns information (OS, total memory, IP address etc.) about the system it is run on.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pysys',
+    version='0.1dev',
+    license='MIT',
+    description='A simple Python package that returns information about the system it is run on.',
+    long_description=open('README.md').read(),
+    install_requires=['psutil'],
+    url='https://github.com/patrickjkennedy/pysys',
+    author='Patrick Kennedy',
+    author_email='patrickken@gmail.com'
+)
+
